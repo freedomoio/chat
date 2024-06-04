e238d591651f5ce4c5097179536dbfc331a977f2
sergioRedmiNote11TPro <sergio@redmiNote11TPro.com>
2024-06-04 23:23:33 +0800

    the third version, 主要修改了Chat.java和Person.java

--- Chat.java
+++ Chat.java
@@ -2,8 +2,13 @@ import java.net.*;
 import java.util.*;
 public class Chat{
     private Chat(){}
-	private static List<Room> list = new ArrayList<>();
+	private static Map<Integer, Room> roomMap = new TreeMap<>();
+	private static Map<Integer, Person> perMap = new TreeMap<>();
 	private static int port = -1;
+	// when some one invoke the method close()
+	// then the boolean will be true
+	// subsequently the program will be closed
+	private static boolean close = false;
 	static{
 		ResourceBundle bundle = ResourceBundle.getBundle("setting");
 		port = bundle.getInt("port");
@@ -12,10 +17,22 @@ public class Chat{
     public static void main(String[] args){
 		ServerSocket ss = null;
 		try{
-			ss = new ServerSocket(port);
-
+			ss = new ServerSocket(port).setTimeOut(60);
+		    while(!close){
+				Socket socket = ss.accept();
+				Person p = new Person(socket);
+			}
 		}catch(IOException e){
 			e.printStackTrace();
 		}
+	}
+	public static void removePer(int id){
+		var per = perMap.get(id);
+		try{
+		    per.socket.close();
+		}catch(IOException e){
+		    e.printStackTrace();
+		}
+		perMap.remove(id);
 	}
 }
--- Person.java
+++ Person.java
@@ -1,9 +1,9 @@
 import java.net.*;
 public class Person implements Runnable{
     private String name;
-	private int id = next_id++;
-	private static int next_id = 0;
-	private Socket socket;
+	private static int nextId = 0;
+	private int id = nextId++;
+	protected Socket socket;
 	public String getName(){
 		return this.name;
 	}
@@ -16,5 +16,54 @@ public class Person implements Runnable{
 	public Person(String name, Socket socket){
 		if(socket == null) return;
 		this.name = name == null ? "null" : name;
+	}
+	public void run(){
+		if(this.socket == null) throw new IllegalStateException("this socket is null");
+		try{
+		    try(Scanner in = new Scanner(socket.getInputStream());
+				PrintWriter out = new PrintWriter(socket.getOutputStream()){
+				out.println("Welcome to Chat Room!");
+				out.println("join <房间号> 以加入房间。");
+				out.print("> ");
+				String cmd;
+				while(in.hasNext()){
+				    cmd = in.next().trim();
+					String[] paras = cmd.split(" ");
+					if("help".equals(paras[0])) out.println(help());
+					else if("quit".equals(paras[0])){
+						if(paras.length == 1) Chat.removePer(id);
+						if(paras.length == 3) checkAdmin(paras);
+					}
+					else if("join".equals(paras[0])){
+						if(paras.length != 2) out.println("错误：参数只能有一个。");
+						try{
+						    Chat.link(Integer.parseInt(paras[1]));
+						}catch(NumberFormatException e){
+						    out.println("输入的参数必须是数字。");
+						}
+					}
+					out.print("> ");
+				}
+		    }
+		}catch(IOException e){
+		    e.printStackTrace();
+		}
+	}
+	private void quit(){}
+	private StringBuilder help(){
+		var b = new StringBuilder();
+		b.append("help 显示本条帮助信息\n");
+		b.append("quit 断开连接(或者程序)\n");
+		b.append("join <房间号> 加入房间\n");
+		b.append("create <房间名> 创建房间");
+		b.append("show 显示房间列表\n");
+		b.append("\t-n 显示当前房间信息\n");
+		b.append("...");
+		return b;
+	}
+	private void checkAdmin(String[] paras){
+		if(paras.length == 3){
+		    if("quit".equals(paras[0]) && "-a".equals()
+		}
 	}
 }
